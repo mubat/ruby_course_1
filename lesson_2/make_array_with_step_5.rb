6dfcddf75cd6f749e53fb843916ee30f2a88a085
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2020-11-01 18:08:06 +0200

    Add code for task #2.2

---
+++ lesson_2/make_array_with_step_5.rb
@@ -0,0 +1,15 @@
+### Заполнить массив числами от 10 до 100 с шагом 5
+result_array = Array.new
+(10..100).each {|i| result_array.push(i) if i % 5 == 0 }
+
+#second variant
+for i in 10..100 do 
+  if i % 5 == 0
+    result_array.push(i)
+  end
+end
+puts result_array
+
+#third variant
+10.upto(100) {|i| result_array.push(i) if i % 5 == 0 }
+puts result_array
