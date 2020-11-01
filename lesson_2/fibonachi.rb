4d5ad78992f030ba8b98240e0abb6b9580d5ffcc
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2020-11-01 19:26:32 +0200

    Add code for task #2.3

---
+++ lesson_2/fibonachi.rb
@@ -0,0 +1,24 @@
+### Заполнить массив числами фибоначчи до 100
+
+fibbonachi = Array.new(100)
+
+fibbonachi.each_index do |i|
+  if(i <= 1)
+    fibbonachi[i] = i
+    next
+  end
+  
+  fibbonachi[i] = fibbonachi[i-1] + fibbonachi[i-2]
+
+end
+
+# secibd variant
+fibbonachi2 = Array.new()
+100.times do |i|
+  if i < 2
+    fibbonachi2[i] = i
+    next
+  end
+  fibbonachi2[i] = fibbonachi2[i-1] + fibbonachi2[i-2]
+
+end
