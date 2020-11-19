7ce1683fa058a0c1671476c38b1ddb2a6e3108d3
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2020-11-19 21:17:34 +0200

    change Route to use array to save the way stations

--- lesson_3/Route.rb
+++ lesson_3/Route.rb
@@ -5,30 +5,31 @@
 # Может выводить список всех станций по-порядку от начальной до конечной
 
 class Route
+  attr_reader :way_stations
 
   def initialize(start_station, end_station)
     @start_station = start_station
     @end_station = end_station
-    @way_stations = {}
+    @way_stations = []
   end
 
   def add_way_station(station)
-    @way_stations[station.name] = station
+    @way_stations.push(station)
   end
 
   def delete_way_station(station)
-    if !@way_stations.has_key?(station.name)
+    if @way_stations.find_index(station.name).nil?
       puts "We already don`t stop on station #{station.name}"
       return
     end
 
-    @way_stations.delete(station.name)
+    @way_stations.reject!(station)
     puts 'Now we skip #{station.name}'
   end
 
   def print_stations
     print @start_station.name + ' -> '
-    @way_stations.each {|name, station|  print @station.name + ' -> '}
+    @way_stations.each {|station|  print @station.name + ' -> '}
     print @end_station.name
   end
 
