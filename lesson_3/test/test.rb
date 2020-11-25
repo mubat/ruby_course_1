05a53d5393b08e694f69854f5a63e8dbd63afd55
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2020-11-25 14:54:26 +0200

    update tests according last changes

--- lesson_3/test/test.rb
+++ lesson_3/test/test.rb
@@ -1,15 +1,19 @@
-require_relative 'Station.rb'
-require_relative 'Route.rb'
-require_relative 'Train.rb'
+require_relative '../Station.rb'
+require_relative '../Route.rb'
+require_relative '../Train.rb'
 
 station_start = Station.new('station_start')
 station_end = Station.new('station_end')
 
 route = Route.new(station_start, station_end)
 
-puts 'stations:'
-route.print_stations
-puts "\n"
+print 'stations: '
+if(route.way_stations.kind_of?(Array)) 
+  puts route.way_stations == [station_start, station_end] ? "[] Expected station list" : "[E] Generated station list has not expected order"
+else 
+  puts "[E] No stations found" 
+end
+
 puts "Start station: " + route.start_station.name
 puts "End station: " + route.end_station.name
 
@@ -25,9 +29,21 @@ train.register_route(route)
 
 station_1 = Station.new('station_1')
 route.add_way_station(station_1)
-puts 'stations:'
-route.print_stations
+print 'stations: '
+if(route.way_stations.kind_of?(Array)) 
+  puts route.way_stations == [station_start, station_1, station_end] ? "[] Expected station list" : "[E] Generated station list has not expected order"
+else 
+  puts "[E] No stations found" 
+end
 
 puts 'Next station after start: ' + train.get_next_station.name
 puts 'Current station: ' + train.current_station.name + ', route\'s start station: ' + route.start_station.name
 puts 'Previous station after start: ' + (train.get_previous_station.nil? ? 'nil' : train.get_previous_station.name)
+
+station_start.send_train(train)
+puts train.current_station == station_1 ? "[] Correct next station in route " : "[E] Train should be on `station_1`. Current: " + train.current_station.name
+station_1.send_train(train)
+puts train.current_station == station_end ? "[] Correct next station in route " : "[E] Train should be on `station_end`. Current: " + train.current_station.name
+
+station_end.send_train(train)
+puts train.current_station == station_end ? "[] Correct next station in route " : "[E] Train should be on `station_end`. Current: " + train.current_station.name
