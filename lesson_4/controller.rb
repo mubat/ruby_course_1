172e7f23544353f20d41057d0b7450ff62d71502
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2020-12-02 23:56:35 +0200

    create controller for app actions

---
+++ lesson_4/controller.rb
@@ -0,0 +1,34 @@
+require_relative 'station'
+
+class Controller
+  
+  def initialize
+    @stations = []
+    @actions = [
+      {'label' => "Добавить станцию", 'action' => :add_station}
+    ]
+  end
+
+  def print_menu
+    puts "Меню:"
+    i = 0
+    @actions.each do |menu_item|
+      i += 1
+      puts "\t#{i}. #{menu_item['label']}"
+    end
+  end
+
+  def has_action?(index)
+    !@actions[index-1].nil?
+  end
+
+  def call(index)
+    method(@actions[index-1]['action']).call
+  end
+
+  def add_station
+    printf "Добавление новой станции.\n\tВведите название станции: " 
+    @stations.push(Station.new(gets.chomp))
+  end
+
+end
--- lesson_4/main.rb
+++ lesson_4/main.rb
@@ -7,35 +7,21 @@
   # - Отцеплять вагоны от поезда
   # - Перемещать поезд по маршруту вперед и назад
   # - Просматривать список станций и список поездов на станции
-def add_station
-  puts "Здесь будет добавляться станция" #TODO implement
-end
+require_relative 'controller'
 
+puts "Чтобы выйти из приложения, в меню введите абракадабру."
 
-menu_def = [
-  {'label' => "Добавить станцию", 'action' => :add_station}
-]
-
-def print_menu(menu_def)
-  puts "Выберите одно из действий:"
-  i = 0
-  menu_def.each do |menu_item|
-    puts "\t#{i+1}. #{menu_item['label']}"
-  end
-  puts "Чтобы выйти из приложения, введите абракадабру."
-end
-
+controller = Controller.new
 loop do
-  print_menu menu_def
-  
-  choise = gets.chomp.to_i
-  puts "Выбрано: #{choise.to_s}";
-  if(menu_def[choise-1].nil?)
-    break
-  end
-  
-  method(menu_def[choise-1]['action']).call
-  
+    controller.print_menu
+    
+    choise = gets.chomp.to_i
+    if(!controller.has_action?(choise))
+      break
+    end
+    
+    controller.call(choise)
+
 end
 
 puts "Приложение завершило работу."
