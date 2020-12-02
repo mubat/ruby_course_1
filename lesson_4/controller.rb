85eb3a175b8e55dc47251558196b8f213a81a450
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2020-12-03 00:28:53 +0200

    add action to register new trains

--- lesson_4/controller.rb
+++ lesson_4/controller.rb
@@ -1,12 +1,16 @@
 require_relative 'station'
+require_relative 'trains/passenger_train'
+require_relative 'trains/cargo_train'
 
 class Controller
   
   def initialize
     @stations = []
+    @trains = []
     @actions = [
       {'label' => "Добавить станцию", 'action' => :add_station},
       {'label' => "Список всех станций", 'action' => :print_stations},
+      {'label' => "Добавить поезд", 'action' => :add_train},
     ]
   end
 
@@ -36,6 +40,25 @@ class Controller
     i = 0
     puts "Список зарегистрированных станций:"
     @stations.each { |station| i += 1; puts "\t#{i}. #{station.name}"}
+  end
+
+  def add_train
+    puts "Добавление поезда."
+    printf "\tВведите номер поезда: "
+    train_number = gets.chomp
+
+    printf "\tКакого типа поезд? 1 - пассажирский, 2 - грузовой: "
+    train_type = gets.chomp.to_i
+    if train_type == 1
+      train = PassengerTrain.new train_number
+    elsif train_type == 2 
+      train = CargoTrain.new train_number
+    else
+      puts "Тип не поддерживается."
+      return
+    end
+
+    @trains.push(train)
   end
 
 end
