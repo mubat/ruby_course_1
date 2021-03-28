f90c1304356bb439c099707561aac41d1b180bcc
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-03-28 18:49:49 +0300

    include validation parameters at Route class

--- lesson_4/route.rb
+++ lesson_4/route.rb
@@ -3,12 +3,17 @@
 # Может добавлять промежуточную станцию в список
 # Может удалять промежуточную станцию из списка
 # Может выводить список всех станций по-порядку от начальной до конечной
+require_relative '../lesson_7/validate'
+require_relative 'station'
 
 class Route
   attr_reader :way_stations
 
+  include Validate
+
   def initialize(start_station, end_station)
     @way_stations = [start_station, end_station]
+    validate
   end
 
   def add_way_station(station)
@@ -38,5 +43,12 @@ class Route
 
   def to_s
     "#{self.start_station.name} - #{self.end_station.name}"
+  end
+
+  protected
+
+  def validate
+    raise "Route should has start and end station" if @way_stations.nil? || @way_stations.length <= 1
+    raise "Number should be a string" if (!@way_stations[0].is_a? Station) || (!@way_stations[@way_stations.length-1].is_a? Station)
   end
 end
