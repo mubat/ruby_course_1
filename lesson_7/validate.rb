9f8ee23c87a730260218e433602f52abef1ff77b
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-03-28 18:14:38 +0300

    create module that has methods to validate classes

---
+++ lesson_7/validate.rb
@@ -0,0 +1,20 @@
+module Validate
+  def self.included(init_class)
+    init_class.send :include, InstanceModule
+  end
+
+  module InstanceModule
+    def valid?
+      validate
+      true
+    rescue
+      false
+    end
+
+    protected 
+
+    def validate
+
+    end
+  end
+end
