eb86a15c63182317c9d22c32df1f46048dc3eb8f
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-05-06 09:40:23 +0300

    add support of Carriage's volume at Cargo carriage

--- lesson_4/carriages/cargo_carriage.rb
+++ lesson_4/carriages/cargo_carriage.rb
@@ -1,6 +1,27 @@
 class CargoCarriage < Carriage
-	
-  def initialize
+	attr_reader :taken_volume
+
+  def initialize(volume)
     @type = 'грузовой'
+    @volume = volume
+    @taken_volume = 0
   end
+
+  # take a part of cargo volume. 
+  # @return [bool] operation result
+  def take_volume(amount)
+    if @volume - @taken_volume > amount
+      return false
+    end
+
+    @taken_volume += amount
+    return true
+  end
+
+  # @return [Integer] left available volume
+  def available_volume
+    @volume - @taken_volume
+  end
+
+
 end
