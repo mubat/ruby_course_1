5bd5f2c8c352b64ab1f4fee4c909cdab2036bfd5
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-05-08 20:21:27 +0300

    rubocop: add disabling of one method for task resolving

--- lesson_3/task_2.make_array_with_step_5.rb
+++ lesson_3/task_2.make_array_with_step_5.rb
@@ -5,10 +5,12 @@ result_array = []
 (10..100).each { |i| result_array.push(i) if (i % 5).zero? }
 
 # second variant
+# rubocop: disable Style/For
 for i in 10..100 do
   result_array.push(i) if (i % 5).zero?
 end
 puts result_array
+# rubocop: enable Style/For
 
 # third variant
 10.upto(100) { |i| result_array.push(i) if (i % 5).zero? }
