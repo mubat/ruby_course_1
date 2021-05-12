bcbebe385fb2325d54e7c550439e7b7fda12ca39
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-05-12 21:41:15 +0300

    rubocop: split long line, remove underline from using attribute

--- lesson_9/errors/validation_error.rb
+++ lesson_9/errors/validation_error.rb
@@ -6,14 +6,14 @@
 class ValidationError < StandardError
   attr_accessor :attribute, :validator, :arguments, :checked_value
 
-
-  def initialize(attribute, checked_value, _msg = nil)
-    super _msg
+  def initialize(attribute, checked_value, msg = nil)
+    super msg
     @attribute = attribute
     @checked_value = checked_value
   end
 
   def to_s
-    "Failed validation for attribute '#{@attribute}'. Arguments #{@arguments.inspect}. Checked value '#{@checked_value.inspect}'"
+    "Failed validation for attribute '#{@attribute}'. " \
+    "Arguments #{@arguments.inspect}. Checked value '#{@checked_value.inspect}'"
   end
 end
